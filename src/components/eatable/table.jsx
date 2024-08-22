080783fff3bc8f36638b0b79df0c4874d40a8ef4
personal2004 <kowsalyakavitha64@gmail.com>
2024-08-22 21:20:47 +0530

    samall error correction in table

--- src/components/eatable/table.jsx
+++ src/components/eatable/table.jsx
@@ -1,16 +1,18 @@
 import './index.css';
 import icons from '../../constants/icons';
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 const Eatable=({tablecon,height,width})=>{
 
     const [searchvalue,setsearchvalue]=useState('');
-    
+    const location=useLocation();
     const filteredEmployees = searchvalue ?tablecon.filter(data =>
             data.name.name.toLowerCase().includes(searchvalue.toLowerCase()) ||
             (data.name?.rollnum || '').toLowerCase().includes(searchvalue.toLocaleLowerCase())
         )
     : tablecon;
 
+    const isleaverequestpage=location.pathname==="/attendance/leaverequest";
     return(
         <div className='eatable_component' style={{height:height,width:width}}>
             <div className='eatable_header'>
@@ -26,11 +28,12 @@ const Eatable=({tablecon,height,width})=>{
                         <th>{tablecon[0].header.col2}</th>
                         <th>{tablecon[0].header.col3}</th>
                         <th>{tablecon[0].header.col4}</th>
-                        <th>{tablecon[0].header.col5}</th>
-                        <th>{tablecon[0].header.col6}</th>
-                        <th style={{color:'#808080'}}>{tablecon[0].header.col7}</th>                       
+                        <th style={isleaverequestpage?{color:'#4F5E74'}:{color:'#808080'}}>{tablecon[0].header.col5}</th>
+                        {isleaverequestpage && <th>{tablecon[0].header.col6}</th>}
+                        {isleaverequestpage &&<th style={{color:'#808080'}}>{tablecon[0].header.col7}</th> }                       
                     </tr>
                     {filteredEmployees.map((data,index)=>{
+                        console.log(data)
                         return(
                             <tr key={index}>
                                 <td>
@@ -45,8 +48,8 @@ const Eatable=({tablecon,height,width})=>{
                                 </td>
                                 <td>{data.date}</td>
                                 <td>{data.designation}</td>
-                                <td>{data?.Reason}</td>
-                                <td>{data?.days}</td>
+                               {isleaverequestpage && <td style={isleaverequestpage?{color:'#4F5E74'}:{display:'none'}}>{data.Reason}</td>}
+                               {isleaverequestpage && <td style={isleaverequestpage?{color:'#4F5E74'}:{display:'none'}}>{data.days}</td>}
                                 <td>{data.status}</td>
                                 <td style={{color:'#808080'}}>{icons.threedot}</td> 
                             </tr>
